03ec8e26ef34d1fd1a63002202a292e9113b73ac
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2018-12-01 15:16:49 +0000

    Add Haskell Template to solve other problems

---
+++ 2018/XX_template/Solutions.hs
@@ -0,0 +1,18 @@
+module Solutions(
+    solution1,
+    solution2
+) where
+
+import Debug.Trace
+
+-- enable/disable debugging
+trace' :: String -> a -> a
+-- trace' s a = trace s a
+trace' s a = a
+
+
+solution1 :: [Integer] -> Integer
+solution1 list = 0
+
+solution2 :: [Integer] -> Integer
+solution2 list = 0
---
+++ 2018/XX_template/tests.hs
@@ -0,0 +1,21 @@
+-- Test Cases
+--
+-- Problem 1
+--
+-- Problem 2
+--
+
+import Text.Printf
+import Solutions
+
+main = do
+    print("Problem 1")
+    test_case 0 0
+    print("Problem 2")
+    test_case 0 0
+    return ()
+
+
+test_case e r = 
+    printf " [%s] - expects %d and returned %d\n" (show (e == r) :: String)  (e :: Integer) (r :: Integer)
+    
