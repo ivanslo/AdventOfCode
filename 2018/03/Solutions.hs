14ff0c88a88396f268d4bc40bc3202dfddc0f998
Ivan Slobodiuk <slobodiu@amazon.co.uk>
2018-12-03 16:03:58 +0000

    2018 | day 3 -- part 1 solved

--- 2018/03/Solutions.hs
+++ 2018/03/Solutions.hs
@@ -11,16 +11,53 @@ trace' :: String -> a -> a
 trace' s a = trace s a
 -- trace' s a = a
 
+-- solution 1
 
 solution1 :: [String] -> Int
-solution1 list = sum $ map parseLine list 
+solution1 list = countPixels $ solveIt  $ map toXYWH $ map parseLine list
 
-parseLine:: String -> Int
-parseLine info = trace'(" window: " ++ show (map (toInt) $ filter ((>0) . length) $ splitOneOf ",:@#x" info)) 0
+
+countPixels :: [[Int]] -> Int
+countPixels pixels = foldl (\acc x -> acc + (length $ filter (>1) x)) 0 pixels
+
+
+solveIt :: [(Int, Int, Int, Int)] -> [[Int]]
+solveIt windows = foldl (\matrix win -> incrementFiles matrix win) (matrix10by10) windows
+
+
+matrix10by10 = replicate 1000 $ replicate 1000 0
+
+
+parseLine:: String -> [Int]
+parseLine info =  map (toInt) $ filter ((>0) . length) $ splitOneOf ",:@#x" info
 -- process code = trace'(" c: " ++ show (code)) 0
 
 toInt :: String -> Int
 toInt x = read x :: Int
+
+toXYWH :: [Int] -> (Int, Int, Int, Int)
+toXYWH (n:x:y:w:h:xs) = (x,y,w,h)
+
+incrementFiles :: [[Int]] -> (Int,Int,Int,Int) -> [[Int]]
+incrementFiles [] _ = []
+incrementFiles (f:fs) (x,y,width,height)
+    | y > 0      =  f:(incrementFiles fs (x,y-1,width, height))
+    | height > 0 =  (incrementInterval f x width):(incrementFiles fs (x,0,width,height-1))
+    | otherwise  = f:fs
+
+incrementInterval :: [Int] -> Int -> Int -> [Int]
+incrementInterval [] _ _ = []
+incrementInterval (x:xs) p width
+    | p > 0  = x:(incrementInterval xs (p-1) (width))
+    | width > 0 = (x+1):(incrementInterval xs 0 (width-1))
+    | otherwise = x:xs
+
+
+
+
+
+
+-- solution 2
 
 solution2 :: [String] -> Int
 solution2 list = 0
--- 2018/03/main.hs
+++ 2018/03/main.hs
@@ -3,9 +3,9 @@ import Solutions
 main = do
     input <- getContents
     print("- Part 1 ")
-    print( solution1 (map (toInt) $ words input ) )
+    print( solution1 $ lines input )
     print("- Part 2 ")
-    print( solution2 (map (toInt) $ words input ) )
+    -- print( solution2 (map (toInt) $ words input ) )
 
 
 toInt :: String -> Integer
