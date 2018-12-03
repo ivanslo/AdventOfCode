78fed721dab7b717538484e55e8fa4bcb7998911
Ivan Slobodiuk <slobodiu@amazon.co.uk>
2018-12-03 13:50:02 +0000

    2018 | day 3 | read input

--- 2018/03/Solutions.hs
+++ 2018/03/Solutions.hs
@@ -4,15 +4,23 @@ module Solutions(
 ) where
 
 import Debug.Trace
+import Data.List.Split
 
 -- enable/disable debugging
 trace' :: String -> a -> a
--- trace' s a = trace s a
-trace' s a = a
+trace' s a = trace s a
+-- trace' s a = a
 
 
 solution1 :: [String] -> Int
-solution1 list = trace("list: " ++ show list) 0
+solution1 list = sum $ map parseLine list 
+
+parseLine:: String -> Int
+parseLine info = trace'(" window: " ++ show (map (toInt) $ filter ((>0) . length) $ splitOneOf ",:@#x" info)) 0
+-- process code = trace'(" c: " ++ show (code)) 0
+
+toInt :: String -> Int
+toInt x = read x :: Int
 
 solution2 :: [String] -> Int
 solution2 list = 0
