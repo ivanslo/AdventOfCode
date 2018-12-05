62c5b2d46cf9a9bc05bb092389a0578404760433
Ivan Slobodiuk <slobodiu@amazon.co.uk>
2018-12-05 17:58:43 +0000

    2018 | day 4 -- reducer skeleton (WIP)

--- 2018/04/Solutions.hs
+++ 2018/04/Solutions.hs
@@ -5,16 +5,29 @@ module Solutions(
 
 import Debug.Trace
 import Data.Time
+import Data.List
 import Text.Regex
 
 data Action = WakesUp | FallAsleep | BeginsShift deriving
-            (Show, Read, Eq, Enum, Bounded)
+            (Show, Read,Ord, Eq, Enum, Bounded)
 data Entry = Entry
     { date :: UTCTime
     , action :: Action
     , guard :: Int
     }
-    deriving (Show, Eq)
+    deriving (Show,Ord, Eq)
+
+data Shift = Shift
+    { guardnr :: Int
+    , report :: [Bool]
+    -- this will help to reduce
+    , number :: Int
+    , what :: Action
+    }
+    deriving (Show, Ord, Eq)
+
+-- instance Ord Entry where
+--     compare a b = compare (date a) (date b)
 
 -- enable/disable debugging
 trace' :: String -> a -> a
@@ -23,17 +36,68 @@ trace' s a = trace s a
 
 entryFromString :: String -> Entry
 entryFromString str = 
-    let (Just matched) = matchRegex (mkRegex "\\[(.*)\\]" ) str
+    let (Just matched) = matchRegex (mkRegex "\\[(.*)\\] (.*)" ) str
         (Just date) =  parseTimeM True defaultTimeLocale "%Y-%m-%d %H:%M" (matched !! 0) :: Maybe UTCTime
-    in Entry date WakesUp 1
+        action = getActionFromStr (matched !! 1)
+        guardNr = if action == BeginsShift then getNumberFromStr (matched !! 1) else 0
+    in Entry date action guardNr
 -- entryFromString = 
     -- where readEntry [date, action, guard] = Entry date action guard
+getActionFromStr :: String -> Action
+getActionFromStr str
+    | letter == 'G' = BeginsShift
+    | letter == 'f' = FallAsleep
+    | letter == 'w' = WakesUp
+    where letter = head str
+
+getNumberFromStr :: String -> Int
+getNumberFromStr str = 
+    let (Just nrStr) = matchRegex (mkRegex "#(.*) begins") str
+        nr = read (nrStr !! 0) :: Int
+    in nr
 
 
 solution1 :: [String] -> Integer
-solution1 list = trace'(show $ map entryFromString list) 0
+solution1 list = trace'(show $ getLists $ sort $ map entryFromString list) 0
+
+getLists :: [Entry] -> [Shift]
+getLists [] = []
+getLists entries = foldl reducerFunction [] entries
+-- getLists entries = foldl (\acc e -> reducerFunction acc e) [] entries
+-- getLists (e:entries) 
+--     | guard e /= 0 = ((guard e), vector60):(getLists entries)
+--     | action e == FallAsleep && action next == WakesUp = (fst e, fillList (snd e) from to):(getLists entries)
+--     ----- e is not. It should be the PREVIOUS LIST CREATED. F***
+--     | otherwise = getLists entries
+--     where next = head entries
+--           from = getMinute (date e)
+--           to = getMinute (date next)
+
+
+
+reducerFunction :: [Shift] -> Entry -> [Shift]
+reducerFunction acc entry
+    | guardNr /= 0  = (Shift guardNr  vector60 0 act):(acc)
+    | otherwise     = acc
+    where guardNr = guard entry
+          act = action entry
+-- reducerFunction acc e = ((0,[True])):acc
+-- reducerFunction acc e 
+--         |
+--         where currenDay = head acc
+
+vector60 = replicate 60 False
+
+getMinute :: UTCTime -> Int
+getMinute date = floor $ toRational $ utctDayTime date / 60
 
 solution2 :: [String] -> Integer
 solution2 list = 0
 
-
+fillList :: [Bool] -> Int -> Int  -> [Bool]
+fillList [] _ _ = []
+fillList (x:xs) from to
+    | from > 0 = x:(fillList xs (from-1) to)
+    | to > 0 = True:(fillList xs 0 (to-1))
+    | otherwise  = x:xs
+-- fillList vector entry = 
