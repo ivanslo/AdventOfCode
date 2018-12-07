75a8edba662a22c7c707f8e11e398b9644898c5d
Ivan Slobodiuk <slobodiu@amazon.co.uk>
2018-12-07 10:03:40 +0000

    2018 | day 7 -- add problem (part 1) and input

---
+++ 2018/07/Solutions.hs
@@ -0,0 +1,18 @@
+module Solutions(
+    solution1,
+    solution2
+) where
+
+import Debug.Trace
+
+-- enable/disable debugging
+trace' :: String -> a -> a
+-- trace' s a = trace s a
+trace' s a = a
+
+
+solution1 :: [String] -> String
+solution1 list = ""
+
+solution2 :: [String] -> String
+solution2 list = ""
---
+++ 2018/07/tests.hs
@@ -0,0 +1,22 @@
+-- Test Cases
+--
+-- Problem 1
+--
+-- Problem 2
+--
+
+import Text.Printf
+import Solutions
+
+main = do
+    input <- getContents
+    print("Problem 1")
+    test_case "CABDFE" (solution1 $ lines input)
+    -- print("Problem 2")
+    -- test_case 0 0
+    return ()
+
+
+test_case e r = 
+    printf " [%s] - expects %s and returned %s\n" (show (e == r) :: String)  (e ::String) (r :: String)
+    
