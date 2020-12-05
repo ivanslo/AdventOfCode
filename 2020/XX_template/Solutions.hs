75b7553fbd074739693f0033e1207ed2eaf18ead
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2020-12-05 14:20:56 +0000

    Update haskell template

--- 2020/XX_template/Solutions.hs
+++ 2020/XX_template/Solutions.hs
@@ -8,17 +8,20 @@ import Debug.Trace
 -- enable/disable debugging
 trace' :: String -> a -> a
 trace' s a = trace s a
--- trace' s a = a
+trace'' s = trace' (show s) s
 
 
 -- ----------------------------- 
 -- SOLUTION 1
-solution1 :: [Int] -> Int
+solution1 :: [String] -> Int
+-- solution1 :: [Int] -> Int
 -- solution1 :: [Integer] -> Integer
-solution1 list = 0
+solution1 xs = 0
+
 
 -- ----------------------------- 
 -- SOLUTION 2
-solution2 :: [Int] -> Int
+solution2 :: [String] -> Int
+-- solution2 :: [Int] -> Int
 -- solution2 :: [Integer] -> Integer
-solution2 list = 0
+solution2 xs = 0
--- 2020/XX_template/tests.hs
+++ 2020/XX_template/tests.hs
@@ -1,17 +1,17 @@
 -- Test Cases
---
--- Problem 1
---
--- Problem 2
---
 
 import Text.Printf
 import Solutions
 
 main = do
     input <- getContents
+    -- Problem 1
     print("Problem 1")
     test_case 0 0
+    --  solution1 (map (toInt) $ words input ) 
+    --  solution1 $ lines input 
+
+    -- Problem 2
     print("Problem 2")
     test_case 0 0
     return ()
@@ -21,6 +21,10 @@ test_case e r =
     printf " [%s] - expects %d and returned %d\n" (show (e == r) :: String)  (e :: Int) (r :: Int)
 -- test_case e r = 
 --     printf " [%s] - expects %d and returned %d\n" (show (e == r) :: String)  (e :: Integer) (r :: Integer)
+
+toInt :: String -> Int
+toInt ('+':xs)  = read xs ::Int
+toInt x         = read x ::Int
     
 castToInteger :: String -> Integer
 castToInteger ('+':xs)  = read xs ::Integer
