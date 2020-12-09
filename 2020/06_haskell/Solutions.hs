d5d099020037df1876cdbcb037d2bcbcde3db9eb
Ivan Slobodiuk <slobodiu@amazon.com>
2020-12-09 18:53:29 +0000

    improve day 6

--- 2020/06_haskell/Solutions.hs
+++ 2020/06_haskell/Solutions.hs
@@ -4,9 +4,8 @@ module Solutions(
 ) where
 
 import Debug.Trace
-import Data.List.Split (splitWhen)
-import Data.List (intersect)
-import Data.Set (toList, fromList)
+import Data.List.Split      (splitWhen)
+import Data.List            (intersect, nub)
 
 -- enable/disable debugging
 trace' :: String -> a -> a
@@ -16,17 +15,12 @@ trace'' s = trace' (show s) s
 -- ----------------------------- 
 -- SOLUTION 1
 solution1 :: [String] -> Int
-solution1 xs = sum $ map length $ map uniques $ map concat $ groups xs
-
+solution1 xs = sum $ map (length . nub . concat) $ groups xs
 
 groups :: [String] -> [[String]]
 groups = splitWhen (=="")
 
-uniques :: Ord a => [a] -> [a]
--- transform the list to a set and to a list again.
-uniques xs = toList $ fromList xs
-
 -- ----------------------------- 
 -- SOLUTION 2
 solution2 :: [String] -> Int
-solution2 xs =  sum $ map length $ map (foldl1 intersect) $ groups xs
+solution2 xs =  sum $ map (length . foldl1 intersect) $ groups xs
