6423fbb9c33b57eee78388d6eb42deeef2b532a8
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2022-12-01 21:10:21 +0000

    day 1 refactors (thanks clippy)

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -11,14 +11,12 @@ fn tests() {
 }
 
 fn main() {
-    {
-        let contents = fs::read_to_string("input.txt").expect("cannot read the file");
-        let input = parse(contents);
-        let res_p1 = part1(&input);
-        let res_p2 = part2(&input);
-        println!("part 1: {res_p1}");
-        println!("part 2: {res_p2}");
-    }
+    let contents = fs::read_to_string("input.txt").expect("cannot read the file");
+    let input = parse(contents);
+    let res_p1 = part1(&input);
+    let res_p2 = part2(&input);
+    println!("part 1: {res_p1}");
+    println!("part 2: {res_p2}");
 }
 
 fn parse(filecontent: String) -> Vec<Vec<u32>> {
@@ -27,31 +25,26 @@ fn parse(filecontent: String) -> Vec<Vec<u32>> {
         .map(|x| x.parse::<String>().unwrap())
         .collect::<Vec<String>>()
         .split(|x| x.is_empty())
-        .map( |group| 
-            group.iter()
-            .map( |x| x.parse::<u32>().unwrap())
-            .collect())
+        .map(|group| group.iter().map(|x| x.parse::<u32>().unwrap()).collect())
         .collect();
     lines
 }
 
-fn part1(numbers: &Vec<Vec<u32>>) -> u32 {
-    match numbers.iter().map(|nrs| nrs.iter().sum::<u32>()).max() {
-        Some(max) => max,
-        None => 0
-    }
+fn part1(numbers: &[Vec<u32>]) -> u32 {
+    numbers
+        .iter()
+        .map(|nrs| nrs.iter().sum::<u32>())
+        .max()
+        .unwrap_or(0)
 }
 
-fn part2(numbers: &Vec<Vec<u32>>) -> u32 {
+fn part2(numbers: &[Vec<u32>]) -> u32 {
     let mut nrs = numbers
-            .iter()
-            .map(|nrs| nrs.iter().sum::<u32>())
-            .collect::<Vec<u32>>();
+        .iter()
+        .map(|nrs| nrs.iter().sum())
+        .collect::<Vec<u32>>();
+
     nrs.sort_by(|a, b| b.cmp(a));
 
-    let mut sum = 0;
-    for i in 0..3 {
-        sum += nrs[i]
-    }
-    sum
+    nrs.iter().take(3).sum()
 }
