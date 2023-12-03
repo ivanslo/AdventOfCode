76280230ce9409f16597e5bb62862ca92428de7b
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2023-12-03 20:36:23 +0000

    change template

--- 2023/xx/src/main.rs
+++ 2023/xx/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     println!("part 2: {res_p2}");
 }
 
-fn parse<T>(filecontent: String) -> Vec<T>
+fn parse<T>(filecontent:) -> Vec<T>
 where
     T: FromStr + Debug,
     <T as FromStr>::Err: Debug,
@@ -39,3 +39,36 @@ fn part1(numbers: &Vec<u32>) -> u32 {
 fn part2(numbers: &Vec<u32>) -> u32 {
     0
 }
+
+// --- alternative
+
+// #[test]
+// fn tests() {
+//     let contents: &str = &fs::read_to_string("input_test.txt").expect("Failure");
+//     let input = parse(contents);
+//     let res_p1 = part1(&input);
+//     let res_p2 = part2(&input);
+//     assert!(res_p1 == 0);
+//     assert!(res_p2 == 0);
+// }
+
+// fn main() {
+//     let contents: &str = &fs::read_to_string("input.txt").expect("Failure");
+//     let input = parse(contents);
+//     let res_p1 = part1(&input);
+//     let res_p2 = part2(&input);
+//     println!("part 1: {res_p1}");
+//     println!("part 2: {res_p2}");
+// }
+
+// fn parse(filecontent: &str) -> Vec<&str> {
+//     filecontent.lines().collect()
+// }
+
+// fn part1(lines: &[&str]) -> u32 {
+//     0
+// }
+
+// fn part2(lines: &[&str]) -> u32 {
+//     0
+// }
